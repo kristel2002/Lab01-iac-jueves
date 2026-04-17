1e6ba01228f898c0082e7bbd91a69ae3a747ac7a
kristel2002 <107360289+kristel2002@users.noreply.github.com>
2026-04-17 01:23:16 +0000

    web.tf se coloca los recursos del contenedor que pasa imagen y esta la configuracion con el api y la web

--- iac/web.tf
+++ iac/web.tf
@@ -1,20 +1,31 @@
+# --- IMÁGENES ---
+resource "docker_image" "img_web" {
+  name = "lab/web"
+  build { context = "./src/web" }
+}
 
+resource "docker_image" "img_api" {
+  name = "lab/api"
+  build { context = "./src/api" }
+}
+
+# --- CONTENEDORES ---
 resource "docker_container" "web" {
   name  = "web-${terraform.workspace}-01"
-  image = "lab/web"
-
-   ports {
-    internal = "80"
+  image = docker_image.img_web.image_id
+  networks_advanced { name = docker_network.app_network.name }
+  ports {
+    internal = 80
     external = var.web_port[terraform.workspace]
   }
 }
 
 resource "docker_container" "api" {
   name  = "api-${terraform.workspace}-01"
-  image = "lab/api"
-
-   ports {
-    internal = "3000"
+  image = docker_image.img_api.image_id
+  networks_advanced { name = docker_network.app_network.name }
+  ports {
+    internal = 3000
     external = var.api_port[terraform.workspace]
   }
 }
