97f913f21e27a1818f0683d6ada16fd247236ad1
kristel2002 <107360289+kristel2002@users.noreply.github.com>
2026-04-17 01:17:14 +0000

    variable.tf sirve para declarar y definir las variables de entrada que utilizará tu infraestructura

--- iac/variable.tf
+++ iac/variable.tf
@@ -1,2 +1,17 @@
-variable "web_port" {}
-variable "api_port" {}
+variable "web_port" {
+  type = map(number)
+  default = {
+    "default" = 8081
+    "dev"     = 8082
+    "prod"    = 80
+  }
+}
+
+variable "api_port" {
+  type = map(number)
+  default = {
+    "default" = 3000
+    "dev"     = 3001
+    "prod"    = 3000
+  }
+}
